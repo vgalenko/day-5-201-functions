ba29863012b3895d5ac90c85c7c2cc94da3b5798
vgalenko <vitaliygalenko@gmail.com>
2017-05-12 15:38:08 -0700

    function1 added

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ function sum(a,b){ //eslint-disable-line
   return [mySum, myMsg];
 }
 
-sum();
+sum(4,7);
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+//testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -26,12 +26,11 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a,b){ //eslint-disable-line
+function testMultiply(a,b){ //eslint-disable-line
 
-}
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
